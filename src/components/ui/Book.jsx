948c4d843d445db2cf1e29fdfd4b4bb6131b622f
Ishaq-H <itzizak@hotmail.com>
2023-07-11 17:21:51 +0100

    skeleton

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,28 +1,70 @@
+import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import Rating from "./Rating";
 import Price from "./Price";
+import Rating from "./Rating";
+
 const Book = ({ book }) => {
+  const [img, setImg] = useState();
+
+  // When we switch routes dont set image to unmounted component
+  const mountedRef = useRef(true);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    console.log(image.src);
+    image.onload = () => {
+      setTimeout(() => {
+        setImg(image);
+      }, 300);
+    };
+    return () => {
+      // When the component unmounts
+      mountedRef.current = false;
+    };
+  }, [book.url]);
+
+  useEffect(() => {
+    if (img) {
+      mountedRef.current = true;
+    }
+  }, [img]);
+
   return (
     <div className="book">
-      <Link to={`/books/${book.id}`}>
-        <figure className="book__img--wrapper">
-          <img src={book.url} alt="" className="book__img" />
-        </figure>
-      </Link>
-      <div className="book__title">
-        <Link to={`/books/${book.id}`} className="book__title__link">
-          {book.title}
-        </Link>
-      </div>
-      <Rating rating={book.rating} />
-      <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+      {!img ? (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      ) : (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img className="book__img" src={img.src} alt="" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            originalPrice={book.originalPrice}
+            salePrice={book.salePrice}
+          />
+        </>
+      )}
     </div>
   );
 };
 
 Book.propTypes = {
-  book: PropTypes.string.isRequired,
+  book: PropTypes.object.isRequired,
 };
 
 export default Book;
